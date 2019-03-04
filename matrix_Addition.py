2d4d9faae72733ab9f6e1db5a35f8a4d0576fa60
amirkhan1092 <amirkhan1092@gmail.com>
2019-03-05 00:16:33 +0530

    multiplication of matrix consecutive 1s count

--- matrix_Addition.py
+++ matrix_Addition.py
@@ -1,35 +1,104 @@
-def matrix_size(A):
-    return len(A),len(A[0])
-A = [[1, 3, 4],
-     [3, 5, 6],
-     [3, 54, 8]]
-
-B = [[2, 9, 0],
-     [3, 6, 4],
-     [3, 5, 9]]
-
-# Add matrix B with Row of matrix A
-C = A+B
-
-print('Size of matrix A ', len(A),'x',len(A[0]))
-
-#Addition
-if matrix_size(A) == matrix_size(B):
-    result = [[0, 0, 0],
-              [0, 0, 0],
-              [0, 0, 0]]
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            result[i][j] = A[i][j] + B[i][j]
-
-else:
-    print('dimension for Addition didnt match ')
 
 
-for i in result:
-    for k in i:
-        print(k,end=' ')
+def add(i):
+    return i+2
 
-    print()
+A = ['2','3','4','54']
+print(A)
+d=map(int,A)
+
+print(list(d))
 
 
+
+# def print_digit():
+#     print('this is digit function ')
+#     print(0,1,2,3,4,5,6,7,8,9)
+#
+# print_digit()
+
+
+
+#
+#
+#
+#
+# def matrix_create(A,r,c):
+#     if len(A) == r*c:
+#         mt = [[] for i in range(r)]
+#         fg = 0
+#         count = 0
+#         for k in A:
+#
+#             mt[fg].append(k)
+#             count +=1
+#             if count == c:
+#                 count=0
+#                 fg += 1
+#
+#         return mt
+#
+#
+#     else:
+#         print('dimension not agree ')
+#         return False
+#
+#
+#
+# H = [2,3,3,6,8,4,3,4,5,4]
+#
+# r,c =eval(input('enter the row and column tob configured r,c '))
+# A=matrix_create(H,r,c)
+# if A == False:
+#     pass
+# else:
+#     for i in A:
+#         for k in i:
+#             print(k,end='\t')
+#         print()
+
+
+
+
+# from builtins import range, print
+#
+#
+# def size(A):
+#     return len(A),len(A[0])
+#
+# A = [[2,4],
+#      [3,4,5],
+#      [3,5]]
+#
+# B = [[2,4,5],
+#      [4,0,6],
+#      [4,7,8]]
+#
+# # if size(A) == size(B):
+# #     result = [[0,0,0],
+# #               [0,0,0],
+# #               [0,0,0]]
+# #     for r in range(len(A)):
+# #         for c in range(len(A[0])):
+# #             result[r][c]=A[r][c] + B[r][c]
+# #
+# # else:
+# #     print('dimension not agree')
+# #
+# # print(result)
+# #
+# # for row in result:
+# #     for col in row:
+# #         print(col,end='\t')
+# #     print()
+# a=[]
+#
+# for j in range(5):
+#     a.append(eval(input('enter the value')))
+#
+#
+# print(a)
+#
+# print(a.index(max(a)))
+# print(a.index(min(a)))
+#
---
+++ matrix_multiplication.py
@@ -0,0 +1,30 @@
+def dimension(A):
+    return len(A),len(A[0])
+
+
+X = [[2, 3, 4], [3, 5, 6]]
+Y = [[2, 6], [2, 8], [4, 6]]
+
+r1, c1 = dimension(X)
+r2, c2 = dimension(Y)
+
+result = [[[0]*2]*2]
+
+
+
+if c1 == r2:
+
+    # iterate through rows of X
+    for i in range(r1):
+        # iterate through columns of Y
+        for j in range(c2):
+            # iterate through rows of Y
+            for k in range(c1):
+                result[i][j] += X[i][k] * Y[k][j]
+
+    for r in result:
+        print(r)
+else:
+    print(' dimension not valid for matrix multiplication ')
+
+print(result)
